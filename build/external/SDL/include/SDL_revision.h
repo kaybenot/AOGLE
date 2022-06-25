d23b798759a4bd8f207d0c686356016793b31d13
kaybenot <107229318+kaybenot@users.noreply.github.com>
2022-06-25 03:36:40 +0200

    Delete build directory

--- build/external/SDL/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/kaybenot/AOGLE.git@eb272528e28f3718c162d548ddfbde3621cc1948"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
